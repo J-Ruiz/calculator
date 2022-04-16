cfbca3ae5aae1f48bc2903cc9fe60c02eb95375e
Jake Ruiz <jwr318@gmail.com>
2022-04-15 20:07:06 -0400

    Figured out how to store the value of each buttons innerhtml. This will be used to push into an array and then evaluate with operators

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-function add (a,b) {
+document.addEventListener("DOMContentLoaded", function(event){
+    let numberButtons = document.getElementsByTagName("button");
+    let buttonsArray = [];
+    for(let i = 0; i<numberButtons.length; i++){
+        buttonsArray.push(numberButtons[i])
+    }
+    buttonsArray.forEach(button=>{
+        button.addEventListener("click", ()=>{
+            console.log(button.innerHTML);
+        })
+    })
+})
+
+//must evaluate 2 numbers with operator between them first and display that number then move on to the next! 
+
+
+function add(a,b) {
     return a+b; 
 }
 
