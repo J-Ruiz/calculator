1064237d8e1ec2ca7d5c510d21c2c8c0ee48fe05
Jake Ruiz <jwr318@gmail.com>
2022-04-25 17:45:12 -0400

    Completed the calculator. Added the logic to take in and calculate two numbers with an operator between them. Added the ability to clear and reset the calculator. Added the ability to use equals to force the calculator to operate. Throws an error if trying to divide by 0

--- script.js
+++ script.js
@@ -1,21 +1,11 @@
-document.addEventListener("DOMContentLoaded", function(event){
-    let numberButtons = document.getElementsByTagName("button");
-    let buttonsArray = [];
-    for(let i = 0; i<numberButtons.length; i++){
-        buttonsArray.push(numberButtons[i])
-    }
-    buttonsArray.forEach(button=>{
-        button.addEventListener("click", ()=>{
-            console.log(button.innerHTML);
-        })
-    })
-})
-
-//must evaluate 2 numbers with operator between them first and display that number then move on to the next! 
+//to do
+    //ensure that decimals round to elimnate long decimal numbers 
 
 
+
+//operations 
 function add(a,b) {
-    return a+b; 
+    return parseFloat(a)+parseFloat(b); 
 }
 
 function subtract(a,b){
@@ -30,6 +20,129 @@ function multiply(a,b){
     return a*b;
 }
 
+//operator function for operations 
 function operate(operator, a, b){
     return operator(a,b); 
 }
+
+//Used to store data from clicked buttons
+let input = [];
+
+
+
+//Logs each button clicked and evaluates how to store it in the input
+document.addEventListener("click", function(event){
+
+    //only fires when it is a button
+    if(event.target.className == "calcButtons"){
+        let value = event.target.innerHTML;
+        let calcDisplay = document.getElementById("calcDisplay");
+        let regex = /[^\-\+\*\/\=]/gi;
+        
+        console.log(input)
+        //Handle clear
+        if(value == "Clear"){
+            input = [];
+            calcDisplay.innerHTML = 0;
+            return;
+        }
+
+        //if user tries to divide by 0
+        if(value == 0 && input[1]=="/"){
+            console.log(value)
+            calcDisplay.innerHTML = "NO DIVIDING BY 0 CHAMP!";
+            input = [input[0],input[1]];
+            return;  
+        }
+
+        //if clicked button is an operator
+        if(value.match(regex)==null){
+            if(input.length==3){
+                solve(input, value);
+            }
+
+            // if two operators are entered consecutively 
+            else if(input[input.length-1].match(regex)==null){
+                input[1]=value;
+            }
+            
+            else{
+                input.push(value);
+            }
+        }
+
+        //if clicked button is a number button
+        else{
+            if(input.length==0){
+                input.push(value)
+            }
+            else if(input[input.length-1].match(regex)==null){
+                input[input.length] = value;
+            }
+            else{
+                input[input.length-1]+=value;
+            }
+
+        }
+    
+    //Sets up the calculator display to match user input without operators
+        if(input[input.length-1].match(regex)!=null){
+            calcDisplay.innerHTML = input[input.length-1];
+        }
+
+        else{
+            calcDisplay.innerHTML = input[0];
+        }
+}
+
+})
+
+
+
+    
+
+
+
+// function to solve for current numbers inputted by user 
+function solve(array, lastButton){
+    let firstNumber = array[0];
+    let operation = array[1];
+    let secondNumber = array[2];
+
+        
+        //when the length of the array is 4, 
+        //operate on the first three values 
+        //use new value to continue calculator sequence 
+        if(array.length==3){
+            let result ;
+            if(operation=="+"){
+                result = operate(add, firstNumber, secondNumber);
+                console.log(result)
+                input = [result.toFixed(3)];
+            }
+            if(operation=="-"){
+                result = operate(subtract, firstNumber, secondNumber);
+                input = [result.toFixed(3)];
+            }
+            if(operation=="*"){
+                result = operate(multiply, firstNumber, secondNumber);
+                input = [result.toFixed(3)];
+            }
+            if(operation=="/"){
+                result = operate(divide, firstNumber, secondNumber);
+                input = [result.toFixed(3)];
+            }
+            
+
+        }
+            input.push(lastButton)
+            
+        
+}
+
+
+
+
+
+
+
